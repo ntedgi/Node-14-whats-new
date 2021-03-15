11869dad15127f8fd40eed5807cf17aaf7314cbd
Naor Tedgi <naor.tedgi@ironsrc.com>
2021-03-15 12:09:40 +0200

    add more notes

--- Async Local Storage/app.js
+++ Async Local Storage/app.js
@@ -1,20 +1,26 @@
-const {withStore} = require('./store.js')
-const {someService} = require('./storeConsumer.js')
+const { withStore } = require('./store.js')
+const { someService } = require('./storeConsumer.js')
 const express = require('express')
 const app = express()
 const port = 12001
 
 
-function myMiddleware(req,res,next){
-    const store = {
-        id:Math.random(),
-        name :"naor"
-    }
-    withStore(store,next)
+function myMiddleware(req, res, next) {
+  const store = {
+    id: Math.random(),
+    name: "naor"
+  }
+  withStore(store, next)
 };
 
 
+function randomMiddleWare(req, res, next) {
+  next()
+}
+
 app.use(myMiddleware)
+
+app.use(randomMiddleWare)
 
 app.get('/', (req, res) => {
   someService()
